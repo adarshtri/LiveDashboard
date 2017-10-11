ef71ff599f7e1dc8966a1ba3bc5b46eebf2ab52e
Adarsh Trivedi <atrivedi@localhost.localdomain>
2017-10-12 01:03:19 +0530

    BarGraphLive Missing ColorChange

---
+++ CPU_StatLiveGraph/LiveBarGraphs.py
@@ -0,0 +1,34 @@
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+from bokeh.client import push_session
+import numpy as np
+
+currency = ['INR', 'USD', 'YEN']
+value = [0, 0, 0]
+
+p = figure(x_range=currency, sizing_mode='stretch_both', title='Currency Monitor',
+           toolbar_location=None, tools="")
+
+
+def update_data():
+    global value, currency
+    value = []
+    for _ in range(3):
+        value.append(np.random.randint(0, 100))
+    new_data = dict(currency=currency, value=value)
+    print(new_data)
+    source.stream(new_data, 3)
+
+
+source = ColumnDataSource(dict(currency=currency, value=value))
+
+p.vbar(source=source,x='currency', top='value', width=0.9)
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+
+session = push_session(curdoc())
+curdoc().add_root(p)
+curdoc().add_periodic_callback(update_data,1000)
+session.show(p)
+session.loop_until_closed(suppress_warning='Stop the python execution.')

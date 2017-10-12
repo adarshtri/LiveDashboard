c4482090e494c0da2c875f53e6d535b92f259ca2
Adarsh Trivedi <atrivedi@localhost.localdomain>
2017-10-13 03:49:35 +0530

    Embed Getting Plot on Another Page

--- Embedding Graphs into Html/streaming.py
+++ Embedding Graphs into Html/streaming.py
@@ -2,7 +2,9 @@ from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.client import push_session
+from bokeh.embed import server_session, components
 import numpy as np
+import json
 
 currency = ['INR', 'USD', 'YEN']
 value = [10, 10, 10]
@@ -28,6 +30,34 @@ p.y_range.start = 0
 
 session = push_session(curdoc())
 curdoc().add_root(p)
-curdoc().add_periodic_callback(update_data, 1000)
+curdoc().add_periodic_callback(update_data,1000)
+
+script = server_session(p, session_id=session.id)
+
+
+sc, div = components(p)
+
+print(div)
+
+string_before = '''<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Bokeh Scatter Plots</title>
+
+        <link rel="stylesheet" href="http://cdn.pydata.org/bokeh/release/bokeh-0.12.6.min.css" type="text/css" />
+        <script type="text/javascript" src="http://cdn.pydata.org/bokeh/release/bokeh-0.12.6.min.js"></script>
+'''
+
+string_middle = '''</head>
+    <body>'''
+
+string_after = '''</body>
+</html>'''
+
+html = open('live-graph.html', 'w')
+html.write(string_before+"\n"+script+string_middle+"\n"+div+"\n"+string_after)
+html.close()
+
 session.loop_until_closed(suppress_warning='Stop the python execution.')
 
